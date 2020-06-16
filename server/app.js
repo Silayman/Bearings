a82cf38db395fbcfb8f5de84c32a9c4bb7177420
Silayman Nagi <silayman.nagi@gmail.com>
2020-06-16 03:10:16 -0400

    Added database connection

---
+++ server/app.js
@@ -0,0 +1,40 @@
+const express = require('express'); //import express
+const app = express(); //invoke express
+const port = 5000; //port to listen on
+const Mongoose = require('mongoose'); //import mongoose
+const {MongoURL} = require('./keys'); //require mongo atlas connect url
+
+
+/*
+* Connect to DB
+*/
+Mongoose.connect(MongoURL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+/*
+* Successful connection
+*/
+Mongoose.connection.on('connected',()=>{
+    console.log("Connected to Mongo Atlas Database!");
+})
+
+/*
+* unsuccessful connection
+* log error
+*/
+Mongoose.connection.on('error',(err)=>{
+    console.log("Error connecting to Mongo Atlas Database:", err);
+})
+
+
+
+
+/*
+ * Listen to given port
+ */
+app.listen(port, ()=>{
+    console.log("Server is running on", port)
+})
+

42165479c6562bb2b6ac18a28485cfc778878190
Silayman Nagi <silayman.nagi@gmail.com>
2020-06-22 02:48:56 -0400

    Feed page UI complete

--- client/src/components/Feed.component.js
+++ client/src/components/Feed.component.js
@@ -2,7 +2,41 @@ import React from 'react';
 
 const Feed = ()=>{
     return(
-        <h1>Feed</h1> 
+        <div className="feed input-field">
+            <div className="card feed-card">
+                <h5>TonyHawk</h5>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1547447134-cd3f5c716030?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"/>
+                </div>
+                <div className="card-content">
+                    <h6>Grindin'</h6>
+                    <i class="material-icons left">favorite</i>
+                    <input type="text" placeholder="Add a comment..."></input>
+                </div>
+            </div>
+            <div className="card feed-card">
+                <h5>TonyHawk</h5>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1547447134-cd3f5c716030?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"/>
+                </div>
+                <div className="card-content">
+                    <h6>Grindin'</h6>
+                    <i class="material-icons left">favorite</i>
+                    <input type="text" placeholder="Add a comment..."></input>
+                </div>
+            </div>
+            <div className="card feed-card">
+                <h5>TonyHawk</h5>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1547447134-cd3f5c716030?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80"/>
+                </div>
+                <div className="card-content">
+                    <h6>Grindin'</h6>
+                    <i class="material-icons left">favorite</i>
+                    <input type="text" placeholder="Add a comment..."></input>
+                </div>
+            </div>
+        </div>
     )
 }
 

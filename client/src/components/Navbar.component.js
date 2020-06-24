fbf5462acb00e87ac8d6d2d8afbd10e9d06b212f
Silayman Nagi <silayman.nagi@gmail.com>
2020-06-24 01:53:34 -0400

    Navbar changes dynamically if user is logged in or not

--- client/src/components/Navbar.component.js
+++ client/src/components/Navbar.component.js
@@ -1,25 +1,38 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { UserContext } from "../App";
+
 const NavBar = () => {
+  const { state, dispatch } = useContext(UserContext);
+  const renderNav = () => {
+    if (state) {
+      return [
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>,
+        <li>
+          <Link to="/post">Create a post</Link>
+        </li>,
+      ];
+    } else {
+      return [
+        <li>
+          <Link to="/accounts/signin">Login</Link>
+        </li>,
+        <li>
+          <Link to="/accounts/signup">Register</Link>
+        </li>,
+      ];
+    }
+  };
   return (
     <nav>
       <div className="nav-wrapper black">
-        <Link to="/" className="brand-logo left">
+        <Link to={state ? "/" : "/accounts/signin"} className="brand-logo left">
           Bearings
         </Link>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
-          <li>
-            <Link to="/accounts/signin">Login</Link>
-          </li>
-          <li>
-            <Link to="/accounts/signup">Register</Link>
-          </li>
-          <li>
-            <Link to="/profile">Profile</Link>
-          </li>
-          <li>
-            <Link to="/post">Create a post</Link>
-          </li>
+          {renderNav()}
         </ul>
       </div>
     </nav>
